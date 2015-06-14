3d34ca1a2004ac92c56f25895561110747b11a6a
Tanya Sager <tanyasager@yahoo.com.au>
2015-06-14 16:41:59 +1000

    Amend register/login/logout terminology

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sign_up', to: 'users#new'
-  get 'sign_in', to: 'sessions#new'
-  get 'sign_out', to: 'sessions#destroy'
+  get 'sign_up', to: 'users#new', path: 'register' 
+  get 'sign_in', to: 'sessions#new', path: 'login' 
+  get 'sign_out', to: 'sessions#destroy', path: 'logout' 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   resources :campuses, only: [:index, :show]
--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,28 +1,28 @@
 require "rails_helper"
 
-feature "user can sign in" do
-  scenario "user can sign in" do
+feature "logging in and out process" do
+  scenario "user can log in" do
     user = create(:user)
 
     visit root_path
-    click_on "Sign in"
+    click_on "Log in"
 
     within "main" do
       fill_in "Email", with: user.email
       fill_in "Password", with: user.password
-      click_on "Sign in"
+      click_on "Log in"
     end
 
     expect(page).to have_content I18n.t("flash.authenticated.signed_in")
     expect(page).to have_content user.first_name
-    expect(page).not_to have_content "Sign in"
+    expect(page).not_to have_content "Log in"
   end
 
   scenario "can see login errors" do
     visit sign_in_path
 
     within "main" do
-      click_on "Sign in"
+      click_on "Log in"
     end
 
     expect(page).to have_content I18n.t("sessions.flashes.failed")
@@ -32,18 +32,18 @@ feature "user can sign in" do
     user = create(:user)
 
     visit root_path
-    click_on "Sign in"
+    click_on "Log in"
 
     within "main" do
       fill_in "Email", with: user.email
       fill_in "Password", with: user.password
-      click_on "Sign in"
+      click_on "Log in"
     end
 
-    click_on "Sign out"
+    click_on "Log out"
 
     expect(page).to have_content I18n.t("flash.authenticated.signed_out")
     expect(page).not_to have_content user.first_name
-    expect(page).to have_content "Sign in"
+    expect(page).to have_content "Log in"
   end
 end
