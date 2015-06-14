0951821eeb1e172762659d9c23e2bf419a5fb213
Tanya Sager <tanyasager@yahoo.com.au>
2015-06-14 16:23:33 +1000

    User can sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'sign_up', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
+  get 'sign_out', to: 'sessions#destroy'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   resources :campuses, only: [:index, :show]
   root to: 'high_voltage/pages#show', id: 'home'
